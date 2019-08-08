177cdba399ca0d35ed3e1c7cb8905c3d05b0ae1e
laine <laine@kaigen.us>
2019-08-08 10:21:59 -0700

    Remove repeated soupPage declaration

--- main.py
+++ main.py
@@ -1,34 +1,39 @@
 import requests
-from contextlib import closing
 from bs4 import BeautifulSoup
+from ebooklib import epub
 
-def getPage(fictionUrl):
+
+def getPage(fictionUrl):  # Todo: error handing
     pageResponse = requests.get(fictionUrl)
-    return pageResponse
-
-def getTitle(pageResponse):
     soupPage = BeautifulSoup(pageResponse.content, 'html.parser')
+    return soupPage
+
+
+def getTitle(soupPage):
     title = soupPage.find('h1', property="name").string
     return title
 
-def getAuthor(pageResponse):
-    soupPage = BeautifulSoup(pageResponse.content, 'html.parser')
+
+def getAuthor(soupPage):
     author = soupPage.find('h4', property="author").find('a').string
     return author
 
-def getCoverUrl(pageResponse):
-    soupPage = BeautifulSoup(pageResponse.content, 'html.parser')
+
+def getCoverUrl(soupPage):
     img = soupPage.find('div', class_="row fic-header").find('img')
     coverUrl = img.get('src')
     if coverUrl.startswith("/"):
         coverUrl = "https://www.royalroad.com" + coverUrl
     return coverUrl
 
+
+
 # gets table that contain partial chapter links
-def getTable(pageResponse):
-    soupPage = BeautifulSoup(pageResponse.content, 'html.parser')
+def getTable(soupPage):
     chapterTable = soupPage.find(id="chapters")
     return chapterTable
+
+
 
 # gets partial links from above table
 def getChPaths(chapterTable):
@@ -37,6 +42,8 @@ def getChPaths(chapterTable):
         chPaths.append(link.get('href'))
     return chPaths
 
+
+
 # turns incomplete chapter paths from the table into full links
 def constructLinks(chPaths):
     chLinks = []
@@ -44,15 +51,27 @@ def constructLinks(chPaths):
         chLinks.append("https://www.royalroad.com" + path)
     return chLinks
 
-fictionId = input("Enter fiction ID here: ")
-fictionUrl = "https://www.royalroad.com/fiction/" + fictionId
 
-pageResponse = getPage(fictionUrl)
+def createBook(title, author, chLinks):
+    chs = []
 
-title = getTitle(pageResponse)
-author = getAuthor(pageResponse)
-coverUrl = getCoverUrl(pageResponse)
 
-chapterTable = getTable(pageResponse)
+fictionUrl = "https://www.royalroad.com/fiction/" + \
+    input("Enter fiction ID here: ")  # Todo: verify this input
+
+soupPage = getPage(fictionUrl)
+
+title = getTitle(soupPage)
+author = getAuthor(soupPage)
+coverUrl = getCoverUrl(soupPage)
+
+chapterTable = getTable(soupPage)
 chPaths = getChPaths(chapterTable)
 chLinks = constructLinks(chPaths)
+
+createBook(title, author, chLinks)
+
+for link in chLinks:
+    print(link)
+
+# print(chLinks)
