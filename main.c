ed09ba5b5cb9fd971f2079841668ac3fd4980680
laine <laine@kaigen.us>
2020-01-30 03:17:15 -0800

    Create makeHandle, correct * placement, fix loop

--- main.c
+++ main.c
@@ -6,13 +6,27 @@
 #include <curl/curl.h>
 #include "functions.h"
 
-int main(int argc, char const *argv[]) {
-  const char* handler = getenv("_HANDLER");
-  const char* taskRoot = getenv("LAMBDA_TASK_ROOT");
-  const char* runtimeAPI = getenv("AWS_LAMBDA_RUNTIME_API");
+CURL *makeHandle(char *url) {
+  CURL *handle = curl_easy_init();
 
-  while (true) {
-    eventData =
+  curl_easy_setopt(handle, CURLOPT_URL, url);
+  curl_easy_setopt(handle, CURLOPT_HTTP_VERSION, CURL_HTTP_VERSION_2TLS);
+  curl_easy_setopt(handle, CURLOPT_USE_SSL, CURLUSESSL_TRY);
+  curl_easy_setopt(handle, CURLOPT_FOLLOWLOCATION, 1L);
+  curl_easy_setopt(handle, CURLOPT_USERAGENT, "App-Scraper");
+
+  return handle;
+}
+
+int main(int argc, char const *argv[]) {
+  //Grab env variables
+  const char *handler = getenv("_HANDLER");
+  const char *taskRoot = getenv("LAMBDA_TASK_ROOT");
+  const char *runtimeAPI = getenv("AWS_LAMBDA_RUNTIME_API");
+
+  //An intentional infinite loop
+  for (;;) {
+    // char *eventData =;
   }
 
   return 0;
