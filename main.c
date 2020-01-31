5db6754a009abedf8912762f9131dcf696ded86c
laine <laine@kaigen.us>
2020-01-30 17:38:06 -0800

    Testing more variable calling

--- main.c
+++ main.c
@@ -26,8 +26,8 @@ int main(int argc, char const *argv[]) {
   const char *runtimeAPI = getenv("AWS_LAMBDA_RUNTIME_API");
 
   // char *url = "http://" + AWS_LAMBDA_RUNTIME_API + "/2018-06-01/runtime/invocation/next";
-  char *urlPrefix = "http://";
-  char *url = urlPrefix"example.com";
+  char *url = "http://";
+  char *url += "example.com";
   CURLcode result = doRequest(url);
 
 
